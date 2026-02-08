ab72ac4828f23cb2cfc4859a40e025ca8f4c76af
AsfarHorani <asfarh281@gmail.com>
2026-02-08 14:32:16 +0100

    Updated the version and the bag file

--- src/common/abstractDataTypes/src/abstractDataTypes/Bag.hpp
+++ src/common/abstractDataTypes/src/abstractDataTypes/Bag.hpp
@@ -1,12 +1,5 @@
-/*
- * Bag.h
- *
- *  Created on: 12.05.2017
- *      Author: frbe5612
- */
-
-#ifndef ABSTRACTDATATPYES_BAG_HPP
-#define ABSTRACTDATATPYES_BAG_HPP
+#ifndef ABSTRACTDATATYPES_BAG_HPP
+#define ABSTRACTDATATYPES_BAG_HPP
 
 #ifdef NDEBUG
 	#define DEBUG_INFO(a)		/**/
@@ -23,226 +16,210 @@
 #include <memory>
 #include <stdexcept>
 #include <vector>
+#include <ranges> // C++20
 
 template <class T>
 class Bag
 {
-	protected:
-		std::vector<std::shared_ptr<T>> m_bag;
+    protected:
+        std::vector<std::shared_ptr<T>> m_bag;
 
-	public:
-		typedef typename std::vector<std::shared_ptr<T>>::iterator iterator;
+    public:
+        typedef typename std::vector<std::shared_ptr<T>>::iterator iterator;
+        typedef typename std::vector<std::shared_ptr<T>>::const_iterator const_iterator;
 
-		Bag()
-		{
-		}
+        Bag() = default;
 
-		Bag(const Bag<T>& b)
-		{
-			insert(b);
-		}
+        Bag(const Bag<T>& b)
+        {
+            insert(b);
+        }
 
-		virtual ~Bag()
-		{
-			this->clear();
-		}
+        virtual ~Bag()
+        {
+            this->clear();
+        }
 
-		Bag<T> * copy() const
-		{
-			return new Bag<T>(*this);
-		}
+        Bag<T> * copy() const
+        {
+            return new Bag<T>(*this);
+        }
 
-		iterator insert(const Bag<T>& b)
-		{
+        // --- OPTIMIZED BULK INSERT ---
+        iterator insert(const Bag<T>& b)
+        {
+            if (!b.empty())
+            {
+                // Optimization: Reserve memory ONCE to prevent multiple reallocations
+                m_bag.reserve(m_bag.size() + b.size());
+                
+                // Removed the "includes" check loop. 
+                // Bags allow duplicates, checking for them is O(N^2) and slow.
+                return m_bag.insert(m_bag.end(), b.cbegin(), b.cend());
+            }
+            return m_bag.end();
+        }
+
+        // --- Standard Iterator Insert ---
+        iterator insert(iterator a, iterator b, iterator c)
+        {
+            // Removed debug loop for performance
+            return m_bag.insert(a, b, c);
+        }
+
+        // --- Standard Insert ---
+        iterator insert(iterator a, const std::shared_ptr<T>& b)
+        {
+            // Removed debug "includes" check.
+            return m_bag.insert(a, b);
+        }
+
+        bool empty() const
+        {
+            return m_bag.empty();
+        }
+
+        std::shared_ptr<T> front() const
+        {
+            return m_bag.front();
+        }
+
+        void clear()
+        {
+            m_bag.clear();
+        }
+
+        unsigned int size() const
+        {
+            return m_bag.size();
+        }
+
+        unsigned int max_size() const
+        {
+            return m_bag.max_size();
+        }
+
+        const std::shared_ptr<T>& operator [] (unsigned int n) const
+        {
+            return m_bag[n];
+        }
+
+        const std::shared_ptr<T>& at(unsigned int n) const
+        {
 #ifndef NDEBUG
-			// The debug version check if an inserted element is already present in the collection.
-			for (auto i = b.cbegin(); i != b.cend(); i++)
-			{
-				if (this->includes(*i))
-				{
-					//	DEBUG_WARNING("Element " << *i << " already present.")
-				}
-			}
+            if (n >= m_bag.size())
+            {
+                throw std::invalid_argument("Bag.hpp: index out of range");
+            }
 #endif
-			return m_bag.insert(m_bag.cend(), b.cbegin(), b.cend());
-		}
+            return m_bag[n];
+        }
 
-		iterator insert(iterator a, iterator b, iterator c)
-		{
-#ifndef NDEBUG
-			// The debug version check if an inserted element is already present in the collection.
-			for (auto i = b; i != c; i++)
-			{
-				if (this->includes(*i))
-				{
-					//	DEBUG_WARNING("Element " << *i << " already present.")
-				}
-			}
-#endif
-			return m_bag.insert(a, b, c);
-		}
+        // --- OPTIMIZED ADD ---
+        virtual void add(const std::shared_ptr<T>& el)
+        {
+            // Removed "includes" check. 
+            // Push back is Amortized O(1). Checking includes is O(N).
+            m_bag.push_back(el);
+        }
 
-		iterator insert(iterator a, const std::shared_ptr<T>& b)
-		{
-#ifndef NDEBUG
-			// The debug version check if an inserted element is already present in the collection.
+        virtual void add(const std::shared_ptr<T>& el, int index)
+        {
+            if(index < 0 || index >= (int)m_bag.size())
+            {
+                m_bag.push_back(el);
+            }
+            else
+            {
+                m_bag.insert(m_bag.begin() + index, el);
+            }
+        }
 
-			if (this->includes(b))
-			{
-				//	DEBUG_WARNING("Element " << b << " already present.")
-			}
-#endif
-			return m_bag.insert(a, b);
-		}
+        virtual void push_back(const std::shared_ptr<T>& el)
+        {
+            m_bag.push_back(el);
+        }
 
-		bool empty()
-		{
-			return m_bag.empty();
-		}
-
-		std::shared_ptr<T> front()
-		{
-			return m_bag.front();
-		}
-
-		void clear()
-		{
-			m_bag.clear();
-		}
-
-		unsigned int size() const
-		{
-			return m_bag.size();
-		}
-
-		unsigned int max_size() const
-		{
-			return m_bag.max_size();
-		}
-
-		const std::shared_ptr<T>& operator [] (unsigned int n) const
-		{
-			return m_bag[n];
-		}
-
-		const std::shared_ptr<T>& at(unsigned int n) const
-		{
-#ifndef NDEBUG
-			if (n < m_bag.size())
-			{
-#endif
-				return m_bag[n];
-#ifndef NDEBUG
-			}
-			throw std::invalid_argument("Bag.hpp: index out of range");
-#endif
-		}
-
-		virtual void add(const std::shared_ptr<T>& el)
-		{
-#ifndef NDEBUG
-			// The debug version check if an inserted element is already present in the collection.
-			if (this->includes(el))
-			{
-				//	DEBUG_WARNING("Element " << el << " already present.")
-			}
-#endif
-			m_bag.push_back(el);
-		}
-
-		virtual void add(const std::shared_ptr<T>& el, int index)
-		{
-			if(index < 0)
-			{
-				this->add(el);
-			}
-			else
-			{
-				this->insert(begin() + index, el);
-			}
-		}
-
-		virtual void push_back(const std::shared_ptr<T>& el)
-		{
-			m_bag.push_back(el);
-		}
-
-		iterator erase(iterator el)
-		{
+        iterator erase(iterator el)
+        {
 			// Calling erase on an empty std::vector results in segmentation fault
-			if(!m_bag.empty())
-			{
-				return m_bag.erase(el);
-			}
+            if(!m_bag.empty())
+            {
+                return m_bag.erase(el);
+            }
 			
-			return m_bag.end();
-		}
+            return m_bag.end();
+        }
 
-		virtual iterator erase(const std::shared_ptr<T>& el)
-		{
-			iterator it = std::find(m_bag.begin(), m_bag.end(), el);
-			
-			// Calling erase on an empty std::vector results in segmentation fault
-			if(!m_bag.empty())
-			{
-				return m_bag.erase(it);
-			}
+        virtual iterator erase(const std::shared_ptr<T>& el)
+        {
+            // C++20 Ranges Optimization
+            auto it = std::ranges::find(m_bag, el);
+            
+            if(it != m_bag.end())
+            {
+                return m_bag.erase(it);
+            }
+            return m_bag.end();
+        }
 
-			return it;
-		}
+        iterator find(const std::shared_ptr<T>& el)
+        {
+            // C++20 Ranges Optimization
+            return std::ranges::find(m_bag, el);
+        } 
 
-		iterator find(const std::shared_ptr<T>& el)
-		{
-			return std::find(m_bag.begin(), m_bag.end(), el);
-		} // find
+        bool includes(const std::shared_ptr<T>& el) const
+        {
+            // C++20 Ranges Optimization
+            return std::ranges::find(m_bag, el) != m_bag.end();
+        }
 
-		bool includes(const std::shared_ptr<T>& el)
-		{
-			return (find(el) != m_bag.end());
-		}
+        int index_of(const std::shared_ptr<T>& el)
+        {
+            auto it = std::ranges::find(m_bag, el);
+            return index_of(it);
+        }
 
-		int index_of(const std::shared_ptr<T>& el)
-		{
-			return index_of(find(el));
-		}
+        int index_of(iterator it)
+        {
+            if(it != m_bag.end())
+            {
+                return std::distance(m_bag.begin(), it);
+            }
+            return -1;
+        }
 
-		int index_of(iterator it)
-		{
-			if(it != m_bag.end())
-			{
-				return (it - m_bag.begin());
-			}
+        template <class U>
+        Bag(Bag<U> const &u)
+        {
+            // Simple copy, relying on vector assignment
+            // (Assumes shared_ptr types are compatible)
+            for(const auto& item : u)
+            {
+                m_bag.push_back(item);
+            }
+        }
 
-			return -1;
-		}
+        virtual const_iterator cbegin() const
+        {
+            return m_bag.cbegin();
+        }
 
-		template <class U>
-		Bag(Bag<U> const &u)
-		{
-			m_bag = u.m_bag;
-		}
+        virtual const_iterator cend() const
+        {
+            return m_bag.cend();
+        }
 
-		typedef typename std::vector<std::shared_ptr<T>>::const_iterator const_iterator;
+        virtual iterator begin()
+        {
+            return m_bag.begin();
+        }
 
-		virtual const_iterator cbegin() const
-		{
-			return m_bag.cbegin();
-		}
-
-		virtual const_iterator cend() const
-		{
-			return m_bag.cend();
-		}
-
-		virtual iterator begin()
-		{
-			return m_bag.begin();
-		}
-
-		virtual iterator end()
-		{
-			return m_bag.end();
-		}
+        virtual iterator end()
+        {
+            return m_bag.end();
+        }
 };
 
 #endif
--- src/util/src/util/crow_all.h
+++ src/util/src/util/crow_all.h
@@ -12176,7 +12176,7 @@ namespace crow
 #if defined CROW_CAN_USE_CPP17 && !defined CROW_GCC83_WORKAROUND
           -> typename std::invoke_result<decltype(&Router::new_rule_tagged<Tag>), Router, std::string&&>::type
 #elif !defined CROW_GCC83_WORKAROUND
-          -> typename std::result_of<decltype (&Router::new_rule_tagged<Tag>)(Router, std::string&&)>::type
+-> std::invoke_result_t<decltype(&Router::new_rule_tagged<Tag>), Router, std::string&&>
 #endif
         {
             return router_.new_rule_tagged<Tag>(std::move(rule));
